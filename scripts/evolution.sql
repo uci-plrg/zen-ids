669eb4abb292ddf3a6a0f8c4bc51221dba5f5b4a
Byron Hawkins <byron@hawkinssoftware.net>
2016-05-06 04:21:25 -0700

    Add connection id for safe multi-threaded feedback of DB changes.

--- scripts/evolution.sql
+++ scripts/evolution.sql
@@ -8,7 +8,8 @@ CREATE TABLE opmon_evolution (
   table_name VARCHAR(24),
   column_name VARCHAR(24),
   id BIGINT(20),
-  PRIMARY KEY (table_name, column_name, id)
+  connection_id int,
+  PRIMARY KEY (table_name, column_name, id, connection_id)
 );
 
 
@@ -22,13 +23,13 @@ CREATE TRIGGER evo_update_wp_commentmeta AFTER UPDATE ON wp_commentmeta
   FOR EACH ROW
   BEGIN
     IF NEW.comment_id != OLD.comment_id THEN
-      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'comment_id', NEW.meta_id);
+      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'comment_id', NEW.meta_id, connection_id());
     END IF;
     IF NEW.meta_key != OLD.meta_key THEN
-      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_key', NEW.meta_id);
+      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_key', NEW.meta_id, connection_id());
     END IF;
     IF NEW.meta_value != OLD.meta_value THEN
-      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_value', NEW.meta_id);
+      REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_value', NEW.meta_id, connection_id());
     END IF;
   END $$
 
@@ -37,9 +38,9 @@ DROP TRIGGER IF EXISTS evo_insert_wp_commentmeta;
 CREATE TRIGGER evo_insert_wp_commentmeta AFTER INSERT ON wp_commentmeta
   FOR EACH ROW
   BEGIN
-    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'comment_id', NEW.meta_id);
-    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_key', NEW.meta_id);
-    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_value', NEW.meta_id);
+    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'comment_id', NEW.meta_id, connection_id());
+    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_key', NEW.meta_id, connection_id());
+    REPLACE INTO opmon_evolution VALUES ('wp_commentmeta', 'meta_value', NEW.meta_id, connection_id());
   END $$
 
 DELIMITER ;
